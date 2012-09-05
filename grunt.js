c43c2504a96cb021f7f2432ad8d1293c492593ee
Andreas Boehrnsen <andreas@boehrnsen.de>
2012-09-05 22:13:00 +0200

    adding grunt-reload

--- grunt.js
+++ grunt.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: '<json:opensong.jquery.json>',
+    
     meta: {
       banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
@@ -11,6 +12,7 @@ module.exports = function(grunt) {
         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
+
     coffee: {
       app: {
         src: ['src/**/*.coffee'],
@@ -23,6 +25,7 @@ module.exports = function(grunt) {
     coffeelint: {
       app: ['src/**/*.coffee']
     },
+    
     concat: {
       dist: {
         src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
@@ -35,16 +38,15 @@ module.exports = function(grunt) {
         dest: 'dist/<%= pkg.name %>.min.js'
       }
     },
+    
     mocha: {
       index: ['specs/index.html']
     },
+    
     lint: {
       files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
     },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint qunit'
-    },
+    
     jshint: {
       options: {
         curly: true,
@@ -63,13 +65,47 @@ module.exports = function(grunt) {
         jQuery: true
       }
     },
+    
+    server: {
+      port: 8000,
+      base: '.'
+    },
+    reload: {
+      port: 3000,
+      proxy: {
+        host: 'localhost',
+        port: '8000'
+      }
+    },
+
+    watch: {
+      coffee: {
+        files: 'src/**/*.coffee',
+        tasks: 'coffee coffeelint'
+      },
+      reload: {
+        files: ['dist/**'],
+        tasks: 'reload'
+      }
+    },
+
     uglify: {}
   });
 
   grunt.loadNpmTasks('grunt-coffee');
   grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-reload');
   grunt.loadNpmTasks('grunt-mocha');
 
+  grunt.registerTask('wait', 'Wait forever.', function() {
+    grunt.log.write('Waiting...');
+    this.async();
+  });
+
   // Default task.
-  grunt.registerTask('default', 'lint qunit concat min');
+  grunt.registerTask('default', 'server reload watch');
+
+  // TODO: add dist and test task
+  //grunt.registerTask('dist', 'server reload watch');
+  //grunt.registerTask('test', 'server reload watch');
 };
