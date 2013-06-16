30256481059a446239f8cef7a5526ce0b336ae8c
Andreas Böhrnsen <andreas@boehrnsen.de>
2013-06-16 15:04:39 +0800

    move inclusions

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,9 +62,9 @@ module.exports = function(grunt) {
 
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-coffee');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
